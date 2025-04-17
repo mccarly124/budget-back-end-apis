83f2a294cd9d6fea586eeba5283a750862042002
Maggie <mccarly124@gmail.com>
2025-04-17 16:45:55 -0400

    Adding cors

--- src/main/java/com/app_budget/Controllers/IncomeController.java
+++ src/main/java/com/app_budget/Controllers/IncomeController.java
@@ -13,14 +13,14 @@ public class IncomeController {
     @Autowired
     private IncomeRepository incomeRepository;
 
- //   @CrossOrigin(origins = "http://localhost:5173")
+    @CrossOrigin(origins = "http://localhost:5173")
     @PostMapping(path="/post")
     public @ResponseBody String addNewTransaction(@RequestBody Income income){
         incomeRepository.save(income);
         return "Added!";
     }
 
- //   @CrossOrigin(origins = "http://localhost:5173")
+    @CrossOrigin(origins = "http://localhost:5173")
     @GetMapping("/get")
     public @ResponseBody Iterable<Income> getAllUsers() {
         return incomeRepository.findAll();
